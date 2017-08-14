cc53d26ed514be96ae94477324105a8ea23b9f87
东哥 <908726964@qq.com>
2017-08-14 12:39:33 +0800

    加了组播

--- c.c
+++ c.c
@@ -1,12 +1,31 @@
 #include<stdio.h>
-
-
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
 
 int main()
 {
+int udp_fd;
+if((udp_fd=socket(AF_INET,SOCK_DGRAM,0))<0)perror("socket error");
+struct sockaddr_in add;
+memset(&add,0,sizeof(add));
+add.sin_addr.s_addr=htonl(INADDR_ANY);
+add.sin_family=AF_INET;
+add.sin_port=htons(2000);
+if(bind(udp_fd,(struct sockaddr*)&add,sizeof(add))!=0)perror("bind error");
 
-printf("adsfasdfasdfsdf\n");
+struct ip_mreq group_add;//设置组播地址结构
+memset(&group_add,0,sizeof(group_add));
+group_add.imr_interface.s_addr=htonl(INADDR_ANY);
+if(inet_pton(AF_INET,"255.0.0.10",&group_add.imr_multiaddr.s_addr)==0)perror("inet_pton error");
 
-
+if(setsockopt(udp_fd,IPPROTO_IP,IP_ADD_MEMBERSHIP,&group_add,sizeof(group_add))!=0)perror("setsockopt error");//加入组播
+char buff[1024];
+while(1)
+{
+ 	recv(udp_fd,buff,1024,0);
+}
  	return 0;
 }
